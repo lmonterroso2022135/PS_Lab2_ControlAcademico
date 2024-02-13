2024adc88da12e87db49bb775350b28061c6b815
lmonterroso2022135 <lmonterroso-2022135@kinal.edu.gt>
2024-02-13 09:46:50 -0600

    Estructura y conexion de la API

--- db/config.js
+++ db/config.js
@@ -1,10 +1,14 @@
-const mongooose = require('mongoose');
+const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_CNN,{});
         console.log('base de datos conectada');
     } catch (e) {
-        
+        throw new Error('Error al conectar la base de datos', e);
     }   
+}
+
+module.exports = {
+    dbConnection
 }
